a558752e1717b595e92a60abaa4ddac794c5347a
adriancelistorres <adriancelistorres184@gmail.com>
2023-08-28 11:00:40 -0500

    final de la prueba update

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 // import Sidebar from './components/layouts/Sidebar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
-  // const hasIdParam = window.location.pathname !== '/' && window.location.pathname !== '';
-  // if (hasIdParam) {
-  //   // Redirect to the main page if there's an 'id' in the URL
-  //   window.location.href = '/';
-  // }
+  const hasIdParam = window.location.pathname !== '/' && window.location.pathname !== '';
+  if (hasIdParam) {
+    // Redirect to the main page if there's an 'id' in the URL
+    window.location.href = '/';
+  }
   return (
     <div className="App">
       <BrowserRouter>
