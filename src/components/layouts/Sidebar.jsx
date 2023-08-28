fe40827ddc0ecfd88a16069e7131ae5bf551f581
adriancelistorres <adriancelistorres184@gmail.com>
2023-08-28 12:30:24 -0500

    Merge commit from dev

--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -115,6 +115,8 @@ function Sidebar({ isLoading, isError, peopleData, getMore }) {
           </div>
         ) : null}
 
+
+
         {/* Mostrar mensaje cuando ya no hay más personajes */}
         {!isLoadingMore &&
           !isLoading &&
